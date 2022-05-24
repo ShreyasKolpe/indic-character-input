b33bd55c4b98b415333d9a23420ebbc8cccef4ea
Shreyas Kolpe <shreyas10.kolpe@gmail.com>
2022-05-24 18:26:49 +0530

    Separated JS into separate file

---
+++ component.js
@@ -0,0 +1,68 @@
+var shadowRoot;
+var textInputToUse = null;
+
+class IndicTextInput extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({mode: 'open'});
+        shadowRoot = this.shadowRoot;
+        const char_map = {
+            0: {'a': 'disabled', 'ā': '_a', 'i': 'disabled', 'ī': '_i', 'u': 'disabled'},
+            1: {'ū': '_u', 'r̥': '_r', 'e': 'disabled', 'ē': '_e', 'ai': 'disabled'},
+            2: {'o': 'disabled', 'ō': '_o', 'ou': 'disabled', 'ṁ': '_m', 'ḥ': '_h'},
+            3: {'k': 'disabled', 'kh': 'disabled', 'g': 'disabled', 'gh': 'disabled', 'ṅ': '_ng'},
+            4: {'c': 'disabled', 'ch': 'disabled', 'j': 'disabled', 'jh': 'disabled', 'ñ': '_ny'},
+            5: {'ṭ': '_t', 'ṭh': 'disabled', 'ḍ': '_d', 'ḍh': 'disabled', 'ṇ': '_N'},
+            6: {'t': 'disabled', 'th': 'disabled', 'd': 'disabled', 'dh': 'disabled', 'n': 'disabled'},
+            7: {'p': 'disabled', 'ph': 'disabled', 'b': 'disabled', 'bh': 'disabled', 'm': 'disabled'},
+            8: {'ṟ': '_R', 'ḷ': '_l', 'ḻ': '_L', 'ś': '_sh', 'ṣ': '_Sh'}
+        }
+
+        const table = document.createElement("table");
+        for(var key in char_map) {
+            const row = document.createElement("tr")
+            table.appendChild(row);
+            for(var elem in char_map[key]) {
+                const cell = document.createElement("td")
+                if(char_map[key][elem] == 'disabled') {
+                    cell.innerHTML = "<input type='button' value='" + elem +"' disabled>";
+                }
+                else {
+                    cell.innerHTML = "<input type='button' value='" + elem + "' id='" + char_map[key][elem] + "' onclick=\"insertCharacterInto(this.id)\">";
+                }
+                row.appendChild(cell);
+            } 
+        }
+
+        this.shadowRoot.append(table);
+        // this.hidden = true
+    }
+}
+customElements.define("indic-text-input", IndicTextInput)
+
+function insertCharacterInto(characterId) {
+    // console.log(characterId, textInputToUse);
+    var elem = document.getElementById(textInputToUse)
+    var charToInsert = shadowRoot.getElementById(characterId).value
+    var curPos = elem.selectionStart
+    var text = elem.value;
+    elem.value = text.slice(0, curPos) + charToInsert + text.slice(curPos);
+    elem.focus()
+    elem.selectionEnd = curPos + 1
+}
+
+function registerthis(inputId) {
+    textInputToUse = inputId
+}
+
+function toUpper() {
+    var elem = document.getElementById(textInputToUse)
+    var curPos = elem.selectionStart;
+    var curEnd = elem.selectionEnd;
+    // console.log(curPos, curEnd);
+    var text = elem.value;
+    elem.value = text.slice(0, curPos) + text.slice(curPos, curEnd).toUpperCase() + text.slice(curEnd);
+    elem.focus()
+    elem.selectionEnd = curEnd
+}
